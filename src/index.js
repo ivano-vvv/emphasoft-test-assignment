2e0bfd4abe242c523602ef381cd2a986b8ee7786
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-12 15:10:28 +0300

    background changed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import * as serviceWorker from "./serviceWorker";
+import "./styles/index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
