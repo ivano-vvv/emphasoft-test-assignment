a05fbf5b6eca00bfc29a731738b1d88a34e2cfe3
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-12 13:25:01 +0300

    basic components added

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "normalize.css";
 import "./styles/app.css";
 import "./styles/typography.css";
+import Content from "./components/containers/app__content";
 
 export default function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Content className="app__content " />
+    </div>
+  );
 }
