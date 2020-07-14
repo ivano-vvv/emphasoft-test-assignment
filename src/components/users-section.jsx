558238a1b39b52d3071692e247170ec26e9936b5
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-14 18:32:31 +0300

    special view for empty list created

--- src/components/users-section.jsx
+++ src/components/users-section.jsx
@@ -8,22 +8,31 @@ export default function UsersSection(props) {
       <header className="users-section__header">
         <h2 className="users-section__heading h2">Пользователи</h2>
       </header>
-      <div className="users-section__list">
-        {props.users.map((u) => {
-          return (
-            <UserCard
-              key={u.id}
-              id={u.id}
-              username={u.username}
-              first_name={u.first_name}
-              last_name={u.last_name}
-              is_active={u.is_active}
-              is_superuser={u.is_superuser}
-              className="users-section__user-card"
-            />
-          );
-        })}
-      </div>
+      <div className="users-section__list">{userListContent(props.users)}</div>
     </section>
   );
+
+  function userListContent(users) {
+    if (!users || users.length === 0)
+      return (
+        <h3 className="h3 users-section__empty-list">
+          Список пользователей пуст
+        </h3>
+      );
+
+    return props.users.map((u) => {
+      return (
+        <UserCard
+          key={u.id}
+          id={u.id}
+          username={u.username}
+          first_name={u.first_name}
+          last_name={u.last_name}
+          is_active={u.is_active}
+          is_superuser={u.is_superuser}
+          className="users-section__user-card"
+        />
+      );
+    });
+  }
 }
