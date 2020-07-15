499a90e9c3750f6d78bcaebda8ab3e05e8f45487
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-15 15:05:09 +0300

    removing token if server gives 401 added

--- src/redux/thunks/usersListThunk.js
+++ src/redux/thunks/usersListThunk.js
@@ -4,15 +4,23 @@ import {
   turnOffLoader,
 } from "../actions/usersListActions";
 import { getUsersAPI } from "../../dataAccess/usersAPI";
+import { logOut } from "../actions/authActions";
 
 export default function getUsers() {
   return (dispatch) => {
     dispatch(turnOnLoader());
-    getUsersAPI().then((response) => {
-      dispatch(setUsers(response.data));
-      dispatch(turnOffLoader());
-    });
-    // f: if auth error turn off isAuth
+    getUsersAPI()
+      .then((response) => {
+        dispatch(setUsers(response.data));
+        dispatch(turnOffLoader());
+      })
+      .catch((error) => {
+        if (error.response) {
+          if (error.response.status === 401) {
+            dispatch(logOut());
+          }
+        }
+      });
     // f: else 'smth went wrong, try it later'
     // f: turn off the loader
   };
