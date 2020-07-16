c8ba0e456d695e9460db5edcb04e4ce037f559d6
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-16 17:55:06 +0300

    user form is styled

--- src/components/user-form.jsx
+++ src/components/user-form.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./user-form.css";
 import { reduxForm, Form, Field } from "redux-form";
 import Input from "./common/input";
 import required from "../validations/required";
@@ -26,72 +27,76 @@ function UserForm(props) {
       className={"user-form " + props.className}
       onSubmit={props.handleSubmit(onSubmit)}
     >
-      <div className="user-form__input-block">
-        <label className="user-form__label text2" htmlFor="username">
-          Имя пользователя
-        </label>
-        <Field
-          className="user-form__input"
-          name="username"
-          component={Input}
-          type="text"
-          validate={[required, maxLength150, usernamePatternValidate]}
-        />
+      <div className="user-form__text-inputs">
+        <div className="user-form__input-block">
+          <label className="user-form__label text2" htmlFor="username">
+            Имя пользователя
+          </label>
+          <Field
+            className="user-form__input"
+            name="username"
+            component={Input}
+            type="text"
+            validate={[required, maxLength150, usernamePatternValidate]}
+          />
+        </div>
+        <div className="user-form__input-block">
+          <label className="user-form__label text2" htmlFor="username">
+            Пароль
+          </label>
+          <Field
+            className="user-form__input"
+            name="password"
+            component={Input}
+            type="password"
+            validate={[required, maxLength128, passwordPatternValidate]}
+          />
+        </div>
+        <div className="user-form__input-block">
+          <label className="user-form__label text2" htmlFor="username">
+            Имя
+          </label>
+          <Field
+            className="user-form__input"
+            name="first_name"
+            component={Input}
+            type="text"
+            validate={maxLength30}
+          />
+        </div>
+        <div className="user-form__input-block">
+          <label className="user-form__label text2" htmlFor="username">
+            Фамилия
+          </label>
+          <Field
+            className="user-form__input"
+            name="last_name"
+            component={Input}
+            type="text"
+            validate={maxLength150}
+          />
+        </div>
       </div>
-      <div className="user-form__input-block">
-        <label className="user-form__label text2" htmlFor="username">
-          Пароль
-        </label>
-        <Field
-          className="user-form__input"
-          name="password"
-          component={Input}
-          type="password"
-          validate={[required, maxLength128, passwordPatternValidate]}
-        />
+      <div className="user-form__submit-block">
+        <div className="user-form__checkbox-block">
+          <Field
+            className="user-form__checkbox"
+            name="is_active"
+            component="input"
+            type="checkbox"
+          />
+          <label className="user-form__label text2" htmlFor="username">
+            Пользователь активен
+          </label>
+        </div>
+        <Button
+          type="submit"
+          isFetching={props.submitting}
+          disabled={!props.valid}
+        >
+          {props.isEditMode ? "Сохранить" : "Добавить"}
+        </Button>
       </div>
-      <div className="user-form__input-block">
-        <label className="user-form__label text2" htmlFor="username">
-          Имя
-        </label>
-        <Field
-          className="user-form__input"
-          name="first_name"
-          component={Input}
-          type="text"
-          validate={maxLength30}
-        />
-      </div>
-      <div className="user-form__input-block">
-        <label className="user-form__label text2" htmlFor="username">
-          Фамилия
-        </label>
-        <Field
-          className="user-form__input"
-          name="last_name"
-          component={Input}
-          type="text"
-          validate={maxLength150}
-        />
-      </div>
-      <div className="user-form__checkbox-block">
-        <Field
-          className="user-form__checkbox"
-          name="is_active"
-          component="input"
-          type="checkbox"
-        />
-        <label className="user-form__label text2" htmlFor="username">
-          Пользователь активен
-        </label>
-      </div>
-      <Button
-        type="submit"
-        isFetching={props.submitting}
-        disabled={!props.valid}
-      >
-        {props.isEditMode ? "Сохранить" : "Добавить"}
-      </Button>
     </Form>
   );
 }
