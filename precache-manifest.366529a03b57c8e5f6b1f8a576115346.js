651271bf1670a019f57bf45cffd41f66268f0781
ivano-vvv <ivanovlad2010@gmail.com>
2020-07-17 17:56:21 +0300

    Updates

---
+++ precache-manifest.366529a03b57c8e5f6b1f8a576115346.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b9ec681515721064b4b181a0982780",
+    "url": "/emphasoft-test-assignment/index.html"
+  },
+  {
+    "revision": "ecf80c5e80a237d73aca",
+    "url": "/emphasoft-test-assignment/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "10c7ec8a33097602613b",
+    "url": "/emphasoft-test-assignment/static/css/main.a3e204d3.chunk.css"
+  },
+  {
+    "revision": "ecf80c5e80a237d73aca",
+    "url": "/emphasoft-test-assignment/static/js/2.d8e37663.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emphasoft-test-assignment/static/js/2.d8e37663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c7ec8a33097602613b",
+    "url": "/emphasoft-test-assignment/static/js/main.232b6ac9.chunk.js"
+  },
+  {
+    "revision": "2bfd913d7c30a02d2831",
+    "url": "/emphasoft-test-assignment/static/js/runtime-main.8e43c701.js"
+  }
+]);
